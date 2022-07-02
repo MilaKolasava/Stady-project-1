01d825db70bcd6195d1615bac7d2796a393223c7
Mila Kolasava <kolasava.dev@gmail.com>
2022-07-02 19:30:02 +0200

    feat: add jumbatron

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import Jumbatron from './Jumbatron';
 
 
 function App() {
   return (
     <>
         <Header />
+        <Jumbatron />
         
     </>
   );
---
+++ src/Jumbatron.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Jumbatron.css';
+
+function Jumbatron() {
+  return (
+    <div className='main-picture'>
+     
+    </div>
+  );
+}
+
+export default Jumbatron;
