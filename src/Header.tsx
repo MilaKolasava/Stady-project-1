244288c7baf1cf3d2c9b538ade37047c3d91e996
Mila Kolasava <kolasava.dev@gmail.com>
2022-07-05 19:14:39 +0200

    fix: delete empty div in header.tsx, reduced div in Jumbotron

--- src/Header.tsx
+++ src/Header.tsx
@@ -4,18 +4,16 @@ import { ReactComponent as Loupe } from "./loupe.svg";
 
 function Header() {
   return (
-    <div className="wrapper">
-      <nav className="navigation">
-        <ul className="list-navigation">
-          <li>HOME</li>
-          <li>BAND</li>
-          <li>TOUR</li>
-          <li>CONTACT</li>
-          <li>MORE</li>
-        </ul>
-        <Loupe className="loupe" width="10" height="10" />
-      </nav>
-    </div>
+    <nav className="wrapper">
+      <ul className="list-navigation">
+        <li>HOME</li>
+        <li>BAND</li>
+        <li>TOUR</li>
+        <li>CONTACT</li>
+        <li>MORE</li>
+      </ul>
+      <Loupe className="loupe" width="10" height="10" />
+    </nav>
   );
 }
 
--- src/Jumbotron.tsx
+++ src/Jumbotron.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Jumbotron.css";
 
 function Jumbotron() {
-  return <div className="main-picture"></div>;
+  return <div className="main-picture" />;
 }
 
 export default Jumbotron;
