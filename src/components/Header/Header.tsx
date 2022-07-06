25649384ee9da7bf632070147ee3f43121ed2149
Mila Kolasava <kolasava.dev@gmail.com>
2022-07-06 21:28:08 +0200

    itr-3: refactor: create MenuItem/MenuItems components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import "./Header.css";
 import { ReactComponent as Loupe } from "../../assets/loupe.svg";
+import MenuItems from "../MenuItems/MenuItems";
+import { MENU_ITEMS } from "./Header.constant";
 
 function Header() {
   return (
     <nav className="wrapper">
-      <ul className="list-navigation">
-        <li>HOME</li>
-        <li>BAND</li>
-        <li>TOUR</li>
-        <li>CONTACT</li>
-        <li>MORE</li>
-      </ul>
+      <MenuItems items={MENU_ITEMS} />
       <Loupe className="loupe" width="10" height="10" />
     </nav>
   );
