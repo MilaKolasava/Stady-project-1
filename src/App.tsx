0ea430a3a62f11eea5622fb1c73e1fc8ed744b86
Mila Kolasava <kolasava.dev@gmail.com>
2022-07-09 17:58:53 +0200

    itr-5: feat: add modal window

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import Jumbotron from "./components/Jumbotron/Jumbotron";
+import ModalWindow from "./components/ModalWindow/ModalWindow";
 
 function App() {
   return (
     <>
       <Header />
       <Jumbotron />
+      <ModalWindow />
     </>
   );
 }
