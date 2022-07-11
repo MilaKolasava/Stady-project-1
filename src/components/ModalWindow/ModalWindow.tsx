c4c35805101148ddb31d51e467b08157bc8c1bdc
Mila <70719733+MilaKolasava@users.noreply.github.com>
2022-07-11 16:31:27 +0200

    itr-5: feat: add modal window (#3)
    * itr-5: feat: add modal window
    
    * itr-5: fix: add empty line between style selectors .css, use selfclosing <div> .tsx
    
    Co-authored-by: Mila Kolasava <kolasava.dev@gmail.com>

---
+++ src/components/ModalWindow/ModalWindow.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ModalWindow.css";
+
+function ModalWindow() {
+  return (
+    <div className="modalWindow">
+      <div className="modalContent" />
+    </div>
+  );
+}
+
+export default ModalWindow;
