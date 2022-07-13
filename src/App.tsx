bcc7858c287905a856b8ad702de188b74ce3c542
Mila Kolasava <kolasava.dev@gmail.com>
2022-07-13 17:28:17 +0200

    itr-6: feat: add closing feature

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <Header setModalActive={setModalActive} />
       <Jumbotron />
-      {modalActive && <ModalWindow />}
+      {modalActive && <ModalWindow setModalActive={setModalActive} />}
     </>
   );
 }
