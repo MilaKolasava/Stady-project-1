295c0dc9cf48ee9ba5cd1d4354a44a5f89d8c959
Mila Kolasava <kolasava.dev@gmail.com>
2022-07-22 13:57:38 +0200

    itR-10: feat: do switching themes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import { ReactComponent as Loupe } from "../../assets/loupe.svg";
 import MenuItems from "../MenuItems/MenuItems";
@@ -7,15 +7,20 @@ import Toggle from "../Toggle/Toggle";
 
 interface HeadersProps {
   setModalActive: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsDarkThemeOff: React.Dispatch<React.SetStateAction<boolean>>;
+  isDarkThemeOff: boolean;
 }
 
 function Header(props: HeadersProps) {
   return (
-    <nav className="wrapper" data-testid="header-wrapper">
+    <nav
+      className={`wrapper wrapper-${props.isDarkThemeOff ? "dark" : "light"}`}
+      data-testid="header-wrapper"
+    >
       <MenuItems items={MENU_ITEMS} />
       <div className="search-and-theme">
         <Loupe
-          className="loupe"
+          className={`loupe-${props.isDarkThemeOff ? "dark" : "light"}`}
           data-testid="header-loupe"
           onClick={() => {
             props.setModalActive(true);
@@ -23,7 +28,10 @@ function Header(props: HeadersProps) {
           width="10"
           height="10"
         />
-        <Toggle />
+        <Toggle
+          setIsDarkThemeOff={props.setIsDarkThemeOff}
+          isDarkThemeOff={props.isDarkThemeOff}
+        />
       </div>
     </nav>
   );
