78449ed099a615271aa51ba3a55070b3ed3e0080
Mila Kolasava <kolasava.dev@gmail.com>
2022-07-28 15:27:41 +0200

    itR-12: refactor: add function getActive

--- src/components/ButtonLang/ButtonLang.tsx
+++ src/components/ButtonLang/ButtonLang.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { LOCALES } from "../../i18n";
+import getActive from "../../utils/getActive";
 import "./ButtonLang.css";
 
 interface ButtonsLangProps {
@@ -11,27 +12,30 @@ function ButtonLang(props: ButtonsLangProps) {
   return (
     <>
       <div
-        className={`button-change button-change-en ${
-          props.locale === LOCALES.ENGLISH ? "active" : ""
-        }`}
+        className={`button-change button-change-en ${getActive(
+          props.locale,
+          LOCALES.ENGLISH
+        )}`}
         onClick={() => props.setLocale(LOCALES.ENGLISH)}
         data-testid="button-en"
       >
         EN
       </div>
       <div
-        className={`button-change button-change-by ${
-          props.locale === LOCALES.BELARUSIAN ? "active" : ""
-        }`}
+        className={`button-change button-change-by ${getActive(
+          props.locale,
+          LOCALES.BELARUSIAN
+        )}`}
         onClick={() => props.setLocale(LOCALES.BELARUSIAN)}
         data-testid="button-by"
       >
         BY
       </div>
       <div
-        className={`button-change button-change-pl ${
-          props.locale === LOCALES.POLISH ? "active" : ""
-        }`}
+        className={`button-change button-change-pl ${getActive(
+          props.locale,
+          LOCALES.POLISH
+        )}`}
         onClick={() => props.setLocale(LOCALES.POLISH)}
         data-testid="button-pl"
       >
