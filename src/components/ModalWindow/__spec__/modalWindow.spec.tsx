4cd50acd3359453c0bf87e24bcbeb3bac1dac0f7
Mila Kolasava <kolasava.dev@gmail.com>
2022-08-03 19:05:07 +0200

    itR-14: refactor: fix existing tests

--- src/components/ModalWindow/__spec__/modalWindow.spec.tsx
+++ src/components/ModalWindow/__spec__/modalWindow.spec.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, screen, cleanup } from "@testing-library/react";
 import ModalWindow from "../ModalWindow";
+import { I18nProvider, LOCALES } from "../../../i18n";
 
 afterEach(() => {
   cleanup();
@@ -8,7 +9,12 @@ afterEach(() => {
 
 test("should render modalwindow", () => {
   const setModalActive = jest.fn;
-  render(<ModalWindow setModalActive={setModalActive} />);
+
+  render(
+    <I18nProvider locale={LOCALES.ENGLISH}>
+      <ModalWindow setModalActive={setModalActive} />
+    </I18nProvider>
+  );
   const modalWindowElement = screen.getByTestId("modal-window");
 
   expect(modalWindowElement).toBeInTheDocument();
@@ -16,7 +22,11 @@ test("should render modalwindow", () => {
 
 test("should render modalcontent", () => {
   const setModalActive = jest.fn;
-  render(<ModalWindow setModalActive={setModalActive} />);
+  render(
+    <I18nProvider locale={LOCALES.ENGLISH}>
+      <ModalWindow setModalActive={setModalActive} />
+    </I18nProvider>
+  );
   const modalWindowElement = screen.getByTestId("modal-content");
 
   expect(modalWindowElement).toBeInTheDocument();
