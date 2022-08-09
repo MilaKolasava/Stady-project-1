9862c59699f29f0eb54c383d127a1c70bd5b36b4
Mila Kolasava <kolasava.dev@gmail.com>
2022-08-09 19:49:42 +0200

    itR-15: refactor: add test

--- src/components/Weather/__spec__/weather.spec.tsx
+++ src/components/Weather/__spec__/weather.spec.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import { I18nProvider, LOCALES } from "../../../i18n";
 import Weather from "../Weather";
 import { mockNavigatorGeolocation } from "./mockNavigatorGeolocation";
@@ -21,14 +21,33 @@ describe("Weather", () => {
   });
 
   describe("render tests", () => {
-    it.skip("should render weather-render-block", () => {
+    it("should render weather-render-block", async () => {
+      const geolocation = {
+        getCurrentPosition: jest.fn().mockImplementationOnce((success) =>
+          Promise.resolve(
+            success({
+              coords: {
+                latitude: 51.1,
+                longitude: 45.3,
+              },
+            })
+          )
+        ),
+      };
+
+      Object.defineProperty(global.navigator, "geolocation", {
+        value: geolocation,
+      });
+
       const { getByTestId } = render(
         <I18nProvider locale={LOCALES.ENGLISH}>
           <Weather />
         </I18nProvider>
       );
 
-      expect(getByTestId("weather-render-block")).toBeInTheDocument();
+      await waitFor(() => {
+        expect(getByTestId("weather-render-bldock")).toBeInTheDocument();
+      });
     });
   });
 });
