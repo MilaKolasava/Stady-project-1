dcdefec474101597a9fefd7406f35519c45c7aaa
Mila Kolasava <kolasava.dev@gmail.com>
2022-09-30 14:56:13 +0200

    itR-20c: refactor: content under modal window do not scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import React, { useState } from "react";
-import message from "react-intl/src/components/message";
+import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import About from "./components/About/About";
-import CityWeather from "./components/CityWeather/CityWeather";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Jumbotron from "./components/Jumbotron/Jumbotron";
 import ModalWindow from "./components/ModalWindow/ModalWindow";
-import Weather from "./components/Weather/Weather";
 import WeatherBlock from "./components/WeatherBlock/WeatherBlock";
 import { I18nProvider, LOCALES } from "./i18n";
 
 function App() {
   const [modalActive, setModalActive] = useState<boolean>(() => false);
   const [locale, setLocale] = useState<string>(() => LOCALES.ENGLISH);
+
+  useEffect(() => {
+    if (modalActive) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "visible";
+    }
+  }, [modalActive]);
 
   return (
     <I18nProvider locale={locale}>
